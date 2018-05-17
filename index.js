1b103b756ff1d9feb64fe09fd206ddae53cb2126
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-17 05:01:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function postComment() {
 function createPost() {
 
   var pageTemplate = _.template(document.getElementById("page-template").innerHTML)
-  var postTemplate =   _.template(document.getElementById("post-template").innerHTML)
-  var commentsTemplate = _.template(document.getElementById("comments-template"))
+  var postTemplate =  _.template(document.getElementById("post-template").innerHTML)
+  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML)
 
   var postTitle = document.getElementById("postTitle").value
   var postAuthor = document.getElementById("postAuthor").value
@@ -30,6 +30,6 @@ function createPost() {
   let postElement = document.getElementById("post");
 
   postElement.innerHTML = postSection
-  postElement.getElementsByTagName('footer')[0].innerHTML += commentsSection
+  postElement.getElementsByTagName('footer')[0].innerHTML = commentsSection
 
 }
